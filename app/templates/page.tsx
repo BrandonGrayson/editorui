614ead60de9e3a222df9ab227009ff4097bb5d86
Brandon Grayson <brandonbgrayson@gmail.com>
2025-09-01 19:34:15 -0400

    templates page loads

---
+++ app/templates/page.tsx
@@ -0,0 +1,8 @@
+import { Typography } from '@mui/material';
+import * as React from 'react';
+
+export default function TemplatesPage() {
+    return (
+        <Typography>List of Templates here</Typography>
+    )
+}
