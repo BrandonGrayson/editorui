f5f922aa23b1d04386412da64ba524cebb75ea35
Brandon Grayson <brandonbgrayson@gmail.com>
2025-09-01 19:30:34 -0400

    homepage with link to templates page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,18 @@
-import Image from "next/image";
+
+import * as React from 'react';
+import { Button, Grid, Typography } from "@mui/material";
+import Link from 'next/link'
 
 export default function Home() {
   return (
-   
-    <p>Hello from the Editor UI</p>
+    <Grid container spacing={2}>
+      <Grid style={{display: 'flex', justifyContent: 'end', height: '100vh', alignItems: 'center'}} size={6}>
+        <Typography>Take your content creation to the next level</Typography>
+      </Grid>
+      <Grid style={{display: 'flex', justifyContent: 'start', height: '100vh', alignItems: 'center'}} size={6}>
+        <Link href={'/templates'}> <Button>Get Started</Button></Link>
+        
+      </Grid>
+    </Grid>
   );
 }
